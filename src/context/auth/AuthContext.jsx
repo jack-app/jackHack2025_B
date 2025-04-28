131b0d8d84dc017c14bcd65d74c94312230a916d
浜田　俊祐 <hamashun0820@icloud.com>
2025-04-28 15:43:05 +0900

    認証を追加

---
+++ src/context/auth/AuthContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "../../firebase/auth";
+
+const AuthContext = createContext({ user: { displayName: "tanaka" } });
+
+const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    onAuthStateChanged((user) => {
+      // ログイン状態が変化すると呼ばれる
+      setCurrentUser(user);
+    });
+  }, []);
+
+  return <AuthContext value={{ user: currentUser }}>{children}</AuthContext>;
+};
+
+export const useAuthContext = () => useContext(AuthContext);
+export default AuthProvider;
---
+++ src/firebase/auth.js
@@ -0,0 +1,45 @@
+import {
+  onAuthStateChanged as onFirebaseAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  getAuth,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export const login = () => {
+  const GoogleProvider = new GoogleAuthProvider();
+  signInWithPopup(auth, GoogleProvider).catch((error) => {
+    console.error("Error during sign-in:", error);
+  });
+};
+
+export const logout = () => {
+  signOut(auth).catch((error) => {
+    console.error("Error during sign-out:", error);
+  });
+};
+
+export const onAuthStateChanged = (callback) => {
+  onFirebaseAuthStateChanged(auth, (user) => {
+    const userInfo = user
+      ? {
+          displayName: user?.displayName,
+        }
+      : null;
+    callback(userInfo);
+  });
+};
