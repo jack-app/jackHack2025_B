753e35005b8700eaa55055e5424d253f4ffe3ed3
shioneko <shioneko37@gmail.com>
2025-05-04 22:18:37 +0900

    構成、useState修正

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,58 +2,64 @@ import React, { useState } from "react";
 import styles from "./Modal.module.css";
 
 const Modal = () => {
-  let date = document.getElementById("date");
-  let tag = document.getElementById("tag");
-  let memo_text = document.getElementById("memo_text");
-
-  const handleRegister = () => {
-    let date_value = date.value;
-    let tag_value = tag.value;
-    let memo_text_value = memo_text.value;
-  };
-
   const [dateValue, setDateValue] = useState("");
   const [tagValue, setTagValue] = useState("");
+  const [memoValue, setMemoValue] = useState("");
+  const [showModal, setShoModal] = useState(true); //後で初期値はfalseに変える
 
-  const handleDate = (e) => {
-    setDateValue(e.target.value);
-  };
-  const handleTag = (e) => {
-    setTagValue(e.target.value);
+  const handleRegister = () => {
+    //setShoModal(false); 登録ボタン押したら非表示にする
+    const data = {
+      date: dateValue,
+      tag: tagValue,
+      memo: memoValue,
+    };
+    console.log(data);
   };
 
-  return (
-    <div className={styles["modal_wrap"]}>
-      <div className={styles["set_info"]}>
+  if (showModal) {
+    return (
+      <div className={styles["modal_wrap"]}>
+        <div className={styles["set_info"]}>
+          <div className={styles["date_area"]}>
+            <input
+              type="text"
+              id="date"
+              className={styles["date"]}
+              value={dateValue}
+              placeholder="いつ"
+              onChange={(e) => setDateValue(e.target.value)}
+            />
+          </div>
+          <div className={styles["tag_area"]}>
+            <input
+              type="text"
+              id="tag"
+              className={styles["tag"]}
+              value={tagValue}
+              placeholder="タグ"
+              onChange={(e) => setTagValue(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className={styles["memo_area"]}>
+          <div className={styles["memo"]}>メモ</div>
+          <textarea
+            id="memo_text"
+            className={styles["memo_text"]}
+            value={memoValue}
+            onChange={(e) => setMemoValue(e.target.value)}
+          ></textarea>
+        </div>
         <input
-          type="text"
-          id="date"
-          className={styles["date"]}
-          value={dateValue}
-          placeholder="いつ"
-          onChange={(e) => handleDate(e)}
-        />
-        <input
-          type="text"
-          id="tag"
-          className={styles["tag"]}
-          value={tagValue}
-          placeholder="タグ"
-          onChange={(e) => handleTag(e)}
+          type="button"
+          className={styles["register_btn"]}
+          value="登録"
+          onClick={handleRegister}
         />
       </div>
-      <div className={styles["memo_area"]}>
-        <div className={styles["memo"]}>メモ</div>
-        <textarea id="memo_text" className={styles["memo_text"]}></textarea>
-      </div>
-      <input
-        type="button"
-        className={styles["register_btn"]}
-        value="登録"
-        onClick={handleRegister}
-      />
-    </div>
-  );
+    );
+  }
 };
 
 export default Modal;
