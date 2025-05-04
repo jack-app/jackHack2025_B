884295e974c00fa9f0a1d13aa9e31bbe7a8f0794
shioneko <shioneko37@gmail.com>
2025-05-04 21:40:16 +0900

    値取得

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,15 +5,23 @@ const Modal = () => {
   let date = document.getElementById("date");
   let tag = document.getElementById("tag");
   let memo_text = document.getElementById("memo_text");
+
   const handleRegister = () => {
-    console.log("clicked");
-    date = date.value;
-    tag = tag.value;
-    memo_text = memo_text.value;
-    console.log(memo_text);
+    let date_value = date.value;
+    let tag_value = tag.value;
+    let memo_text_value = memo_text.value;
   };
-  const handleDate = () => {};
-  const handleTag = () => {};
+
+  const [dateValue, setDateValue] = useState("");
+  const [tagValue, setTagValue] = useState("");
+
+  const handleDate = (e) => {
+    setDateValue(e.target.value);
+  };
+  const handleTag = (e) => {
+    setTagValue(e.target.value);
+  };
+
   return (
     <div className={styles["modal_wrap"]}>
       <div className={styles["set_info"]}>
@@ -21,15 +29,17 @@ const Modal = () => {
           type="text"
           id="date"
           className={styles["date"]}
-          value="いつ"
-          onChange={handleDate}
+          value={dateValue}
+          placeholder="いつ"
+          onChange={(e) => handleDate(e)}
         />
         <input
           type="text"
           id="tag"
           className={styles["tag"]}
-          value="タグ"
-          onChange={handleTag}
+          value={tagValue}
+          placeholder="タグ"
+          onChange={(e) => handleTag(e)}
         />
       </div>
       <div className={styles["memo_area"]}>
