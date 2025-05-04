d472036e2b513fa5fe09d1ee6878fee24add621c
shioneko <shioneko37@gmail.com>
2025-05-04 16:54:39 +0900

    modal途中

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,17 +2,46 @@ import React, { useState } from "react";
 import styles from "./Modal.module.css";
 
 const Modal = () => {
+  let date = document.getElementById("date");
+  let tag = document.getElementById("tag");
+  let memo_text = document.getElementById("memo_text");
+  const handleRegister = () => {
+    console.log("clicked");
+    date = date.value;
+    tag = tag.value;
+    memo_text = memo_text.value;
+    console.log(memo_text);
+  };
+  const handleDate = () => {};
+  const handleTag = () => {};
   return (
-    <div className="modal_wrap">
-      <div className="set_info">
-        <input type="text" className="date" value="いつ" />
-        <input type="text" className="tag" value="タグ" />
+    <div className={styles["modal_wrap"]}>
+      <div className={styles["set_info"]}>
+        <input
+          type="text"
+          id="date"
+          className={styles["date"]}
+          value="いつ"
+          onChange={handleDate}
+        />
+        <input
+          type="text"
+          id="tag"
+          className={styles["tag"]}
+          value="タグ"
+          onChange={handleTag}
+        />
       </div>
-      <div className="memo_area">
-        <div className="memo">メモ</div>
-        <textarea className="memo_text"></textarea>
+      <div className={styles["memo_area"]}>
+        <div className={styles["memo"]}>メモ</div>
+        <textarea id="memo_text" className={styles["memo_text"]}></textarea>
       </div>
-      <input type="button" className="register_btn" value="登録" />
+      <input
+        type="button"
+        className={styles["register_btn"]}
+        value="登録"
+        onClick={handleRegister}
+      />
     </div>
   );
 };
